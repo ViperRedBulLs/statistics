4c037fef982c8c31ac0260b05596c35de2cefe4f
ViperRedBulLs <fathfajhri40@gmail.com>
2022-06-15 05:56:31 +0700

    Added testing file

--- statistics_test.go
+++ statistics_test.go
@@ -12,10 +12,12 @@ var (
 	dataMedian float64 = 3.5
 	dataMin    float64 = 0
 	dataMax    float64 = 9
+	dataSum    float64 = 33
+	dataAvg    float64 = 4.125
 )
 
 // Testing Median
-func TestMedianAray(t *testing.T) {
+func TestMedianArray(t *testing.T) {
 	t.Log("Testing Median")
 
 	if statistics.Median(data) != dataMedian {
@@ -34,5 +36,23 @@ func TestMaxArray(t *testing.T) {
 	t.Log("Testing Max")
 	if statistics.Max(data) != dataMax {
 		t.Errorf("Error `Max` of `%v` should `%v`", data, dataMax)
+	}
+}
+
+// Sum
+func TestSum(t *testing.T) {
+	t.Log("Testing Sum")
+
+	if statistics.Sum(data) != dataSum {
+		t.Errorf("Error `SUM` of `%v` should `%v`", data, dataSum)
+	}
+}
+
+// Average
+func TestAverage(t *testing.T) {
+	t.Log("Testing Average")
+
+	if statistics.Average(data) != dataAvg {
+		t.Errorf("Error `AVERAGE` of `%v` should `%v`", data, dataSum)
 	}
 }
